1dff521898e256eb32d4b5f20652fb97d56e8fd0
thinleyjamtsho-tijtech <67371936+thinleyjamtsho-tijtech@users.noreply.github.com>
2021-09-14 15:18:56 +0600

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! this is for test purpose"); 
+        System.out.println("Hello, World! thimn,mn,ms is for test purpose"); 
     }
 }
