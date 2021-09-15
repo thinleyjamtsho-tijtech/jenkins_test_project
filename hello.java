993be54ff19f53f63a6cb0f66e5d04a0adbf3b04
thinleyjamtsho-tijtech <67371936+thinleyjamtsho-tijtech@users.noreply.github.com>
2021-09-15 14:25:21 +0600

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class SumOfNumbers1
 {  
     public static void main(String args[])   
     {  
-        int n1 = 200, n2 = 150, sum;  
+        int n1 = 200, n2 = 10, sum;  
         sum = n1 + n2;  
         System.out.println("The sum of numbers is: "+sum);  
     }  
