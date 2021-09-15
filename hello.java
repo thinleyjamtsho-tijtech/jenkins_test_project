e2e158b8df1997230cd57c2f44cc0bc36413ba2f
thinleyjamtsho-tijtech <67371936+thinleyjamtsho-tijtech@users.noreply.github.com>
2021-09-15 11:25:26 +0600

    changed code

--- hello.java
+++ hello.java
@@ -1,6 +1,9 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World! This is for test purpose");
-        sdfsd
-    }
-}
+public class SumOfNumbers1  
+{  
+    public static void main(String args[])   
+    {  
+        int n1 = 225, n2 = 115, sum;  
+        sum = n1 + n2;  
+        System.out.println("The sum of numbers is: "+sum);  
+    }  
+}  
